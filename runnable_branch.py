0466b13ce89462aba02b79a6190b4bdcde8abea9
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2026-03-11 07:49:09 +0530

    Refactor import statements and code structure

--- runnable_branch.py
+++ runnable_branch.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
 
 load_dotenv()
 
@@ -30,6 +30,7 @@ branch_chain = RunnableBranch(
 final_chain = RunnableSequence(report_gen_chain, branch_chain)
 
 print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
+
 
 
 
