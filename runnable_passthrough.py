69372bb421912f037364d5ae16b9dc1956ea97a0
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2026-03-11 07:48:12 +0530

    Update runnable_passthrough.py

--- runnable_passthrough.py
+++ runnable_passthrough.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
 
 load_dotenv()
 
@@ -28,5 +28,6 @@ parallel_chain = RunnableParallel({
 })
 
 final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
 
 print(final_chain.invoke({'topic':'cricket'}))
