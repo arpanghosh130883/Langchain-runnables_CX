ef6a9a3df26e26c7fea236e06b4ef3a2bcc952cf
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2026-03-11 07:48:51 +0530

    Refactor imports and clean up runnable_parallel.py

--- runnable_parallel.py
+++ runnable_parallel.py
@@ -1,18 +1,8 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableParallel
-
-
-####    For latest version of Langchain use the below import ### 
-'''
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableSequence, RunnableParallel
 from dotenv import load_dotenv
-''''
 
 load_dotenv()
 
@@ -39,5 +29,6 @@ result = parallel_chain.invoke({'topic':'AI'})
 
 print(result['tweet'])
 print(result['linkedin'])
+
 
 
